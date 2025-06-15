de5c2cdfb4375d6fafe2e33eb41945e4d38b914f
Fred <github@tuxmania.fr>
2025-06-15 14:08:39 +0200

    Put "Spotify" back into place to avoid an error realted to empty variable DEFAULT_OPTIONAL_APPS

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,7 +1,7 @@
 # Only ask for default desktop app choices when running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
-  DEFAULT_OPTIONAL_APPS=''
+  DEFAULT_OPTIONAL_APPS='Spotify'
   export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 fi
 
