2ce0b5c3e805c454e1d6af2f591aff2a40e8cdd5
Fred <github@tuxmania.fr>
2025-06-15 14:19:19 +0200

    Adding log file output

--- install/terminal.sh
+++ install/terminal.sh
@@ -4,4 +4,8 @@ sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
 # Run terminal installers
-for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
+# for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
+for installer in ~/.local/share/omakub/install/terminal/*.sh; do
+  echo "Lancement du script $(basename "$installer")" >> /home/fred/logs.txt
+  source "$installer"
+done
