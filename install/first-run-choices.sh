d3f892a14162b6965438c10f0ee3bbccb2a4fd1a
Fred <github@tuxmania.fr>
2025-06-15 13:16:55 +0200

    Changing default parameters

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,14 +1,14 @@
 # Only ask for default desktop app choices when running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
-  DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
+  DEFAULT_OPTIONAL_APPS=''
   export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 fi
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="Node.js"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
-SELECTED_DBS="MySQL,Redis"
+SELECTED_DBS="PostgreSQL"
 export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
